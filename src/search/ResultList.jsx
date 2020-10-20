c90ec3b1b4fc031b2e806b275a117f180d91ce48
Rainer Simon <rainer.simon@ait.ac.at>
2020-10-20 11:45:55 +0200

    Search results style

--- src/search/ResultList.jsx
+++ src/search/ResultList.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
 import { FixedSizeList as List } from 'react-window';
-import { Ripple } from 'react-onsenui';
+import { Icon, Ripple } from 'react-onsenui';
 import AutoSizer from 'react-virtualized-auto-sizer';
+
+import './ResultList.scss';
+
+const ICONS = {
+  'crm:E18_Physical_Thing': 'md-pin',
+  'crm:E21_Person': 'md-account',
+  'crm:E74_Group': 'md-accounts'
+}
 
 const ResultList = props => {
 
-  const Row = ({ index, style }) =>
-    <div 
+  const Row = ({ index, style }) => {
+
+    const item = props.results[index];
+    console.log(item);
+
+    return (<div 
       className="search-result" 
       style={style}
       onClick={() => props.onSelect(props.results[index])}>
 
       <Ripple modifier="light-gray" />
 
-      {props.results[index].properties.title}
-    </div>
+      <Icon icon={ICONS[item.crmClass]} />
+
+      <label>{item.properties.title}</label>
+    </div>)
+  }
 
   return (
     <div className="search-results">
@@ -23,7 +38,7 @@ const ResultList = props => {
           <List 
             height={height}
             itemCount={props.results.length}
-            itemSize={35}
+            itemSize={58}
             width={width}>
             {Row}
           </List>
