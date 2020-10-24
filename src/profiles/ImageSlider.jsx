4a21c569414227a5fcf5b191818c8f997af17008
Rainer Simon <rainer.simon@ait.ac.at>
2020-10-24 10:01:39 +0200

    Prototype lightbox integration

--- src/profiles/ImageSlider.jsx
+++ src/profiles/ImageSlider.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AliceCarousel from 'react-alice-carousel';
+import Lightbox from 'react-image-lightbox';
 
 import 'react-alice-carousel/lib/alice-carousel.css';
+import 'react-image-lightbox/style.css';
 
 const getValidDepictions = depictions => depictions ? 
   depictions.filter(d => d.url.indexOf('http') === 0) : [];
@@ -11,25 +13,34 @@ export const hasDepictions = item =>
 
 const ImageSlider = props => {
 
+  const [ selected, setSelected ] = useState(null);
+
   const depictions = getValidDepictions(props.depictions);
 
-  const images = depictions.map(d =>
+  const images = depictions.map((d, idx) =>
     <div className="image-wrapper">
       <div className="inner">
-        <img src={d.url} alt={d.title} />
+        <img src={d.url} alt={d.title} onClick={() => setSelected(idx)} />
       </div>
     </div>
   )
 
   return (
-    <div className="image-carousel">
-      <AliceCarousel 
-        mouseTracking
-        disableDotsControls
-        disableButtonsControls
-        autoWidth
-        items={images} />
-    </div>
+    <>
+      <div className="image-carousel">
+        <AliceCarousel 
+          mouseTracking
+          disableDotsControls
+          disableButtonsControls
+          autoWidth
+          items={images} />
+      </div>
+
+      { selected && 
+        <Lightbox 
+          mainSrc={depictions[selected].url} />
+      }
+    </>
   )
 
 }
