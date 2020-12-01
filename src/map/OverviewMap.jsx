72414cdd65bf8e6867a739eb9cce92de82637dae
Rainer Simon <rainer.simon@ait.ac.at>
2020-12-01 12:09:26 +0100

    Proof of concept overview map arrows

--- src/map/OverviewMap.jsx
+++ src/map/OverviewMap.jsx
@@ -1,9 +1,25 @@
 import React, { useEffect, useRef } from 'react';
 import { Map, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
+import centroid from '@turf/centroid';
 import PageWithMenu from '../PageWithMenu';
 import { useI18N } from '../i18n';
 
+import 'leaflet-swoopy';
+
 import './OverviewMap.scss';
+
+const drawArrow = (path, map) => {
+  const start = centroid(path.begins).geometry.coordinates.reverse();
+  const end = centroid(path.ends).geometry.coordinates.reverse();
+
+  L.swoopyArrow(start, end, {
+    color:'#4e89ff',
+    weight:2,
+    factor:-0.5,
+    hideArrowHead:true
+  }).addTo(map);
+}
 
 const OverviewMap = props => {
 
@@ -11,12 +27,11 @@ const OverviewMap = props => {
 
   const mapRef = useRef();
 
-  console.log(props.store.lifePaths);
-
   useEffect(() => {
     if (mapRef.current) {
       const map = mapRef.current.leafletElement;
       map.fitBounds(props.store.geoBounds);
+      props.store.lifePaths.forEach(l => drawArrow(l, map));
     }
   });
 
