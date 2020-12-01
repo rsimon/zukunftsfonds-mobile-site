714042cb410c2cb84ae68557474b5710ac799dd8
Rainer Simon <rainer.simon@ait.ac.at>
2020-12-01 13:51:40 +0100

    Proof of concept: clickable overview map

--- src/map/OverviewMap.jsx
+++ src/map/OverviewMap.jsx
@@ -4,21 +4,26 @@ import L from 'leaflet';
 import centroid from '@turf/centroid';
 import PageWithMenu from '../PageWithMenu';
 import { useI18N } from '../i18n';
+import { navigateTo } from '../profiles/Utils';
 
 import 'leaflet-swoopy';
 
 import './OverviewMap.scss';
 
-const drawArrow = (path, map) => {
+const drawArrow = (path, map, onClick) => {
   const start = centroid(path.begins).geometry.coordinates.reverse();
   const end = centroid(path.ends).geometry.coordinates.reverse();
 
-  L.swoopyArrow(start, end, {
+  const arrow = L.swoopyArrow(start, end, {
     color:'#4e89ff',
     weight:2,
     factor:-0.5,
     hideArrowHead:true
   }).addTo(map);
+
+  // Attaching click handler to the SVG path element directly,
+  // since there seems to be no other way in Swoopy
+  arrow._currentPath.addEventListener('click', onClick);
 }
 
 const OverviewMap = props => {
@@ -27,11 +32,14 @@ const OverviewMap = props => {
 
   const mapRef = useRef();
 
+  const onClick = path =>
+    navigateTo(path.actor, props.navigator);
+
   useEffect(() => {
     if (mapRef.current) {
       const map = mapRef.current.leafletElement;
       map.fitBounds(props.store.geoBounds);
-      props.store.lifePaths.forEach(l => drawArrow(l, map));
+      props.store.lifePaths.forEach(l => drawArrow(l, map, onClick(l)));
     }
   });
 
