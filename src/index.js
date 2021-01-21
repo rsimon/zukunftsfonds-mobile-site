e69ad7004a1d5aa2e226bd795f60745f9bb363d2
Rainer Simon <aboutgeo@gmail.com>
2021-01-21 11:40:49 +0100

    Fixed broken Leaflet icon paths

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ import L from 'leaflet';
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+    iconRetinaUrl: 'images/leaflet/marker-icon-2x.png',
+    iconUrl: 'images/leaflet/marker-icon.png',
+    shadowUrl: 'images/leaflet/marker-shadow.png'
 });
 
 // A simple start helper that loads the data and then switches to the app
