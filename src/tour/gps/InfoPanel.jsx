c132682db584c466d11f7f9ea991b80f7a11fb57
Rainer Simon <aboutgeo@gmail.com>
2021-01-26 15:15:32 +0100

    Basic waypoint info panel

--- src/tour/gps/InfoPanel.jsx
+++ src/tour/gps/InfoPanel.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Icon } from 'react-onsenui';
 import distance from '@turf/distance';
 
 import './InfoPanel.scss';
@@ -8,20 +9,37 @@ const InfoPanel = props => {
   const waypoint = props.tour.waypoints.features[props.waypoint];
 
   // turf.js needs GeoJSON feature
-  const currentPos = {
+  const currentPos = props.pos ? {
     type: 'Feature',
     properties: {},
     geometry: {
       type: 'Point',
       coordinates: [ props.pos.coords.longitude, props.pos.coords.latitude ]
     }
-  };
+  } : null;
 
-  const dist = Math.round(distance(currentPos, waypoint) * 1000);
+  const dist = currentPos ? Math.round(distance(currentPos, waypoint) * 1000) : null;
 
   return (
     <div className="tour-map-infopanel">
-      {dist}
+      <div 
+        className="waypoint-image"
+        style={{ backgroundImage: `url('tours/images/${waypoint.properties.images[0]}')` }}>
+        <h2>{waypoint.properties.title}</h2>
+      </div>
+      <div className="waypoint-description">
+        <p>
+          {waypoint.properties.description}
+        </p>
+
+        <div className="shade-gradient" />
+      </div>
+      <footer className="tour-map-footer">
+        <Icon icon="md-walk" />
+        <span className="distance-to-waypoint">
+           Distance {dist || '-'}m
+        </span>
+      </footer>
     </div>
   ) 
 
