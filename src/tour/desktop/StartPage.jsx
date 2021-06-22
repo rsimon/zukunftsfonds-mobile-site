b275e5280936d20ae3d79c7be969e1cc586e971b
Rainer Simon <aboutgeo@gmail.com>
2021-06-22 13:25:51 +0200

    Hybrid layout for Tour start page

--- src/tour/desktop/StartPage.jsx
+++ src/tour/desktop/StartPage.jsx
@@ -1,16 +1,74 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { GeoJSON, Map, TileLayer } from 'react-leaflet';
+import { Button, Icon } from 'react-onsenui';
 import { useI18N } from '../../i18n';
+import { loadTour } from '../Tour';
 import PageWithMenuDesktop from '../../PageWithMenuDesktop';
+
+import './StartPage.scss';
+
+const PATH_STYLE = {
+  color: '#000',
+  dashArray: '6 6'
+}
 
 const StartPage = props => {
 
   const i18n = useI18N();
 
+  const mapRef = useRef();
+
+  const [ tour, setTour ] = useState();
+
+  useEffect(() => {
+    loadTour('oberhollabrunn', props.store).then(setTour);
+  }, []);
+
+  useEffect(() => {
+    if (mapRef.current && tour) {
+      const map = mapRef.current.leafletElement;
+      map.fitBounds(tour.bounds, { padding: [ 15, 15 ]});
+    }
+  }, [ tour ]);
+
   return (
     <PageWithMenuDesktop
+      className="tour-startpage-desktop"
       title={i18n('Walking Tour')}
       current="Tour"
       {...props}>
+
+      {tour &&
+        <>
+          <div className="tour-desktop-header">
+            <div className="tour-image">
+              <img src={tour.image} alt="GPS walk header decoration" />
+            </div>
+
+            <div className="tour-overview-map">
+              <Map 
+                ref={mapRef}
+                zoomControl={false}
+                attributionControl={false}
+                style={{height:'350px'}}>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                <GeoJSON data={tour.track} {...PATH_STYLE} />
+              </Map>
+              <div className="clicktrap" />
+            </div>
+          </div>
+
+          <div className="tour-description" dangerouslySetInnerHTML={{
+            __html: tour.description
+          }} />
+
+          <div className="start-tour">
+            <Button>
+              <Icon icon="md-walk" /> <label>{i18n('Start the tour')}</label>
+            </Button>
+          </div>
+        </>
+      }
     </PageWithMenuDesktop>
   )
 
